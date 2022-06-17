499f82d1f2857846c0a8073ae6b52a6f2fee40d3
Viktor Sandberg <sandtain@gmail.com>
2022-06-17 18:05:54 +0200

    Initial linter and formatter fix

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  bracketSpacing: true,
+  bracketSameLine: true,
+  singleQuote: false,
+  trailingComma: "all",
+  arrowParens: "avoid",
+  useTabs: false,
+  printWidth: 120,
+  importOrder: ["^react", "^next|next/(.*)$", "^components/(.*)$", "^[./]"],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  importOrderSeparation: true,
+  endOfLine: "auto",
+};
