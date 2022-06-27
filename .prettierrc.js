52430abc44b54f37a2861887b5a966af91a691b5
Viktor Sandberg <sandtain@gmail.com>
2022-06-27 15:26:03 +0200

    Third party last from imports

--- .prettierrc.js
+++ .prettierrc.js
@@ -6,7 +6,7 @@ module.exports = {
   arrowParens: "avoid",
   useTabs: false,
   printWidth: 120,
-  importOrder: ["^react", "^next|next/(.*)$", "^components/(.*)$", "^[./]"],
+  importOrder: ["^react", "^next|next/(.*)$", "<THIRD_PARTY_MODULES>", "^components/(.*)$", "^[./]"],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
   importOrderSeparation: true,
