c1fb0e355a16714c16be43f08ca1b3fa0429ba45
Viktor Sandberg <sandtain@gmail.com>
2022-07-04 11:03:46 +0200

    Update Navbar to not use SC

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,27 @@
 import React from "react";
 
-import { NavbarContainer as Container } from "./Navbar.styled";
+import { Typography } from "@mui/material";
+
+// import { NavbarContainer as Container } from "./Navbar.styled";
 
 function Navbar() {
   return (
-    <Container>
-      <a href="#">Home</a>
-      <a href="#">News</a>
-      <a href="#">About</a>
-    </Container>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        gap: 6,
+        alignItems: "center",
+        height: "50px",
+        color: "white",
+        padding: 12,
+      }}>
+      <div>
+        <a href="#">Home</a>
+        <a href="#">News</a>
+        <a href="#">About</a>
+      </div>
+    </div>
   );
 }
 
