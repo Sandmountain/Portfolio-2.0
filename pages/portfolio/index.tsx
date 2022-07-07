8700835c61e3e00b65757a579ee2f4f47e7390a0
Viktor Sandberg <sandtain@gmail.com>
2022-07-07 15:31:07 +0200

    Change starting route to be /portfolio

--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -1,16 +1,15 @@
-import { Suspense, useEffect, useState } from "react";
+import { useEffect } from "react";
 
 import type { NextPage } from "next";
 
 import getUuid from "uuid-by-string";
 
-import HorizontalProjectDisplay from "../src/components/HorizontalProjectDisplay/HorizontalProjectDisplay";
-import { getImages } from "../src/components/HorizontalProjectDisplay/handleProjects";
-import Navbar from "../src/components/Navbar/Navbar";
-import ThreeLoader from "../src/components/ThreeLoader/ThreeLoader";
-import { useProjectStore } from "../src/mobx/projectStore";
-import { Project, ProjectImageType } from "../src/types/Project";
-import { initContentful } from "../src/utils/contentful/contentful";
+import HorizontalProjectDisplay from "../../src/components/HorizontalProjectDisplay/HorizontalProjectDisplay";
+import { getImages } from "../../src/components/HorizontalProjectDisplay/handleProjects";
+import ThreeLoader from "../../src/components/ThreeLoader/ThreeLoader";
+import { useProjectStore } from "../../src/mobx/projectStore";
+import { Project, ProjectImageType } from "../../src/types/Project";
+import { initContentful } from "../../src/utils/contentful/contentful";
 
 const Home: NextPage<{ projects: Project[]; projectImages: ProjectImageType[] }> = ({ projects, projectImages }) => {
   const projectStore = useProjectStore();
@@ -22,7 +21,6 @@ const Home: NextPage<{ projects: Project[]; projectImages: ProjectImageType[] }>
 
   return (
     <div style={{ height: "100%" }}>
-      <Navbar />
       <ThreeLoader>
         <div style={{ height: "calc(100vh - 50px)", background: "black", color: "white" }}>
           <HorizontalProjectDisplay images={projectImages} projects={projects} />
