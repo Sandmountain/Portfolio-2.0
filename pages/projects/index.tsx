72d4bfb2ab0f512806f39be241ac37c46ba8b8f7
Viktor Sandberg <sandtain@gmail.com>
2022-07-08 14:49:23 +0200

    Make /project route useable

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 import Image from "next/image";
-
-import { Box } from "@mui/material";
+import Link from "next/link";
 
 import { Project } from "../../src/types/Project";
 import { initContentful } from "../../src/utils/contentful/contentful";
+
+import { Box } from "@mui/material";
 
 interface ProjectsPageProps {
   projects: Project[];
@@ -13,21 +14,26 @@ interface ProjectsPageProps {
 
 const ProjectsPage: React.FC<ProjectsPageProps> = ({ projects }) => {
   return (
-    <Box component="div" sx={{ display: "flex", flexWrap: "wrap" }}>
-      {projects.map((project, idx) => {
-        return (
-          <Box key={idx} component="div" sx={{ height: 172, width: 300, position: "relative" }}>
-            <Image
-              key={project.title}
-              src={`http:${project.thumbnail.fields.file.url}`}
-              objectFit="contain"
-              layout="fill"
-              alt={project.title}
-            />
-          </Box>
-        );
-      })}
-    </Box>
+    <>
+      <Box component="div" className="project-grid">
+        {projects.map((project, idx) => {
+          return (
+            <Link key={project.title} href={`/projects/${project.title.toLocaleLowerCase().replaceAll(" ", "-")}`}>
+              <Box component="div" sx={{ height: 172, width: 300, position: "relative" }} className="project-card">
+                <Image
+                  className="zoomContent-image"
+                  key={project.title}
+                  src={`http:${project.thumbnail.fields.file.url}`}
+                  objectFit="cover"
+                  layout="fill"
+                  alt={project.title}
+                />
+              </Box>
+            </Link>
+          );
+        })}
+      </Box>
+    </>
   );
 };
 
