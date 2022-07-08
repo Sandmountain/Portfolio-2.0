0476def25594765a8327e089c47b94ad01d1e7aa
Viktor Sandberg <sandtain@gmail.com>
2022-07-08 12:08:49 +0200

    Add some styling to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,26 +1,44 @@
 import React from "react";
 
-import { Typography } from "@mui/material";
+import { Avatar, Box, Divider, Typography, useTheme } from "@mui/material";
 
 // import { NavbarContainer as Container } from "./Navbar.styled";
 
 function Navbar() {
+  const theme = useTheme();
+
   return (
     <div
       style={{
         display: "flex",
-        justifyContent: "center",
+
         gap: 6,
         alignItems: "center",
         height: "50px",
         color: "white",
         padding: 12,
       }}>
-      <div>
+      <Box component="div" sx={{ display: "flex", alignItems: "center", marginRight: "auto" }}>
+        <Avatar
+          component="div"
+          src="http://localhost:3000/_next/image?url=http%3A%2F%2Fimages.ctfassets.net%2Fpesye57ju1o0%2F6lYyNcTJcDY6yqWvy0jh7L%2Fd69f47794544c1f3ca3197b186d2d6e1%2Flogo.png&w=640&q=75"
+          sx={{ width: 24, height: 24, marginRight: theme.spacing(1) }}
+        />
+        <Divider orientation="vertical" flexItem sx={{ backgroundColor: "rgba(255, 255, 255, 0.4)" }}></Divider>
+        <Box component="div" sx={{ display: "flex", flexDirection: "column", justifyContent: "space-between" }}>
+          <Typography variant="caption" lineHeight={1} sx={{ ml: theme.spacing(1) }}>
+            viktorsandberg.com
+          </Typography>
+          <Typography variant="caption" lineHeight={1} fontSize={8} sx={{ ml: theme.spacing(1) }}>
+            a portfolio page
+          </Typography>
+        </Box>
+      </Box>
+      <Box component="div" sx={{ display: "flex", gap: 2 }}>
         <a href="/portfolio">Home</a>
         <a href="#">News</a>
         <a href="#">About</a>
-      </div>
+      </Box>
     </div>
   );
 }
