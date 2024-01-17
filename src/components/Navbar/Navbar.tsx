86e7e28fe1586569a575ca9f45050e75dced367b
Sandmountain <sandtain@gmail.com>
2024-01-17 13:39:13 +0100

    Change logo to use local image

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -12,20 +12,27 @@ function Navbar() {
   return (
     <div
       style={{
+        position: "relative",
         display: "flex",
         gap: 6,
         alignItems: "center",
         height: "50px",
         color: "white",
         padding: 12,
+        zIndex: 2,
       }}>
       <Link href="/">
         <Box component="div" sx={{ display: "flex", alignItems: "center", marginRight: "auto", cursor: "pointer" }}>
           <Avatar
             component="div"
-            src="http://localhost:3000/_next/image?url=http%3A%2F%2Fimages.ctfassets.net%2Fpesye57ju1o0%2F6lYyNcTJcDY6yqWvy0jh7L%2Fd69f47794544c1f3ca3197b186d2d6e1%2Flogo.png&w=640&q=75"
-            sx={{ width: 24, height: 24, marginRight: theme.spacing(1) }}
+            src="./logo.png"
+            sx={{
+              width: 24,
+              height: 24,
+              marginRight: theme.spacing(1),
+            }}
           />
+
           <Divider orientation="vertical" flexItem sx={{ backgroundColor: "rgba(255, 255, 255, 0.4)" }} />
           <Box component="div" sx={{ display: "flex", flexDirection: "column", justifyContent: "space-between" }}>
             <Typography variant="overline" lineHeight={1} sx={{ ml: theme.spacing(1) }}>
